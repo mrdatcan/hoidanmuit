d73cfb1c9402673c68879fd607b6e229da7f1d6a
mrdatcan <datcanlmht@gmail.com>
2025-07-27 18:49:08 +0700

    Initial push

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService, // Assuming ConfigService is imported and used
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get() //route
+  @Render("home")
+  handleHomePage() {
+    //port from .env
+    console.log(">>>Check PORT = ",this.configService.get<string>('PORT'));
+    const messsage =this.appService.getHello();
+    // return this.appService.getHello();
+    return{
+      
+    }
   }
 }
